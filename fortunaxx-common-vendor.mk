e257c0fcff43fa91ebf5eaa8eb44175edba06fe3
ZIM <zim555@mail.ru>
2016-11-17 19:51:50 +0200

    Add common vendor files

--- fortunaxx-common-vendor.mk
+++ fortunaxx-common-vendor.mk
@@ -3,13 +3,9 @@ DEVICE_PACKAGE_OVERLAYS := vendor/samsung/fortunaxx-common/overlay
 
 PRODUCT_PACKAGES += \
     libtime_genoff\
-    TimeService
-
-PRODUCT_PACKAGES += \
-    libmm-abl
-
-PRODUCT_PACKAGES += \
-    libHevcSwDecoder
+    TimeService \
+	libmm-abl \
+	libHevcSwDecoder
 
 # Include non-open-source files
 $(call inherit-product, vendor/samsung/fortunaxx-common/fortunaxx-common-vendor-blobs.mk)
